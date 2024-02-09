0cf1fffd8f398767621d49799b313b9196b4bc7b
ABuBakar7447 <lone7447@gmail.com>
2024-02-10 04:09:57 +0600

    all donation post show in large device made responsive

--- src/app/alldonation/page.js
+++ src/app/alldonation/page.js
@@ -9,7 +9,7 @@ const Alldonations = async () => {
             <p className="text-center text-5xl my-16 text-cyan-300 font-sans font-bold">
                 Donate to Save Huminity 
             </p>
-            <div className="grid grid-cols-4 w-5/6 mx-auto">
+            <div className="grid grid-cols-3  w-5/6 mx-auto">
                 {data.map(donations => <DonationCard key={donations._id} donations={donations}></DonationCard>)}
             </div>
         </div>
