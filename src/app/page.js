b39e52d00170a189598bcf774f3f0d2c644f5c24
ABuBakar7447 <lone7447@gmail.com>
2024-02-23 00:07:24 +0600

    2 section has been commented

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
       <Banner></Banner>
       <Activity></Activity>
       <AllDonation></AllDonation>
-      <OurPromise></OurPromise>
-      <Volunteers></Volunteers>
+      {/* <OurPromise></OurPromise> */}
+      {/* <Volunteers></Volunteers> */}
     </div>
   );
 }
