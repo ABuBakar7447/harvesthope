bd10c32eb002ea57729b363198a694f5441e15a2
ABuBakar7447 <lone7447@gmail.com>
2024-02-23 00:09:34 +0600

    1 section uncommented

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Banner></Banner>
       <Activity></Activity>
       <AllDonation></AllDonation>
-      {/* <OurPromise></OurPromise> */}
+      <OurPromise></OurPromise>
       {/* <Volunteers></Volunteers> */}
     </div>
   );
